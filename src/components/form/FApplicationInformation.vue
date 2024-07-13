8d75cf1b6dab1a8354a25cd494567ed76e8acde6
晴天雨露 <clovme@163.com>
2024-07-13 18:23:28 +0800

    完成报考信息编辑设置

--- src/components/form/FApplicationInformation.vue
+++ src/components/form/FApplicationInformation.vue
@@ -9,7 +9,7 @@ const data = getStore<IApplicationInfo>('getApplicationInfo');
 <template>
   <div>
     <div class="split-row-module">
-      <el-row class="split-row-1">
+      <el-row class="split-row-1" style="margin-bottom: 15px">
         <el-col :span="5">
           <div class="split-1">
             <label>报考院校</label>
@@ -22,24 +22,26 @@ const data = getStore<IApplicationInfo>('getApplicationInfo');
             <el-input v-model="data.major" placeholder="请输入专业名称" clearable />
           </div>
         </el-col>
-        <el-col :span="14">
-          <div class="split-1" style="justify-content: flex-end">
-            <el-button class="el-c-button" :icon="plus" plain>添加专业课成绩</el-button>
-          </div>
-        </el-col>
+        <el-col :span="14"></el-col>
       </el-row>
-      <el-row class="split-row-1">
-        <el-col v-if="data.gradeGrade.length > 0">
+      <el-row class="split-row-1" v-if="data.gradeGrade.length > 0">
+        <el-col style="margin-bottom: 15px">
           <div class="split-1">
             <el-row>
               <el-col :span="24">
                 <table class="baokao-table">
                   <tr>
-                    <td rowspan="2">{{data.cname}}</td>
-                    <td v-for="item in data.gradeGrade">{{ item.name }}</td>
+                    <td rowspan="2">
+                      <input type="text" v-model="data.cname" />
+                    </td>
+                    <td v-for="item in data.gradeGrade">
+                      <input type="text" v-model="item.name" />
+                    </td>
                   </tr>
                   <tr>
-                    <td v-for="item in data.gradeGrade">{{ item.score }}</td>
+                    <td v-for="item in data.gradeGrade">
+                      <input type="text" v-model="item.score" />
+                    </td>
                   </tr>
                   <tr>
                     <td>删除</td>
@@ -50,6 +52,16 @@ const data = getStore<IApplicationInfo>('getApplicationInfo');
                 </table>
               </el-col>
             </el-row>
+          </div>
+        </el-col>
+        <el-col style="margin-bottom: 15px">
+          <el-divider border-style="dashed" style="border-color: #ffc69f"><b>表格数据可任意编辑</b></el-divider>
+        </el-col>
+        <el-col :span="11">
+          <div class="split-1">
+            <el-input placeholder="专业课名称，例如：英语" clearable/>:
+            <el-input placeholder="专业课成绩，例如：80" clearable/>
+            <el-button class="el-c-button" :icon="plus" plain>添加专业课成绩</el-button>
           </div>
         </el-col>
       </el-row>
@@ -63,11 +75,34 @@ table.baokao-table {
 
   td {
     border: 1px solid #aaa;
-    padding: 5px;
     text-align: center;
     font-weight: 400;
-    min-width: 70px;
+    width: 80px;
+    height: 27px;
     white-space: nowrap;
+
+    input {
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      min-width: auto;
+      max-width: unset;
+    }
+
+    input[type="text" i] {
+      padding-block: 0;
+      padding-inline: 0;
+    }
+
+    input,
+    input:active,
+    input:hover,
+    input:focus,
+    input:focus-visible,
+    input:focus-visible{
+      border: none;
+      outline: none;
+    }
   }
 
   tr:last-child {
